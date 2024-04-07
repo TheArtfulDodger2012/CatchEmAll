22925b9e812bdef3bef6ad52f1f1cd1b456e4de1
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-07 14:43:57 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Ron Lane on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
